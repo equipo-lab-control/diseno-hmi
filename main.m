069930c433a2d91d482fd5bec3714090ec311825
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-14 10:52:43 -0500

    Got transfer function

--- main.m
+++ main.m
@@ -1,44 +1,34 @@
 clear 
 
 syms s
-t0 = 0;
-t1 = 6;
+t0 = 11;
+t1 = 15;
 
-temp0 = 24.476
+temp0 = 25.494;
 
-tempf = 143.186
+tempf = 142.896;
 
-dtemp = tempf - temp0
+dtemp = tempf - temp0;
 
-temp = (0.632 * dtemp) + temp0
+temp = (0.632 * dtemp) + temp0;
 
-t2 = 66.5
+t2 = 77;
 
-tau = t2 - t1
+tau = t2 - t1;
 
 % manipularcion
-dm = 50 - 0 
+dm = 50 - 0 ;
 
-K = dtemp / (dm)
+K = dtemp / (dm);
 
 
 theta = t1 - t0;
 
 
+s = tf('s');
 
-G(s) = K / (tau * s + 1) * exp(-theta * s)
+G = K / (tau * s + 1) * exp(-theta * s);
 
-
-
-
-
-
-
-
-
-
-
-
-
+step(G);
 
 
